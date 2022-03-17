3a6d492192366a480cbb8cfc85ad5e14e06fbfb1
Luxor44 <92293868+Luxor44@users.noreply.github.com>
2022-03-17 13:48:03 +0100

    Update client.py

--- lorashit/esp/client.py
+++ lorashit/esp/client.py
@@ -1,5 +1,24 @@
 from time import sleep
 from ulora import LoRa, ModemConfig, SPIConfig
+from machine import Pin
+import dht
+
+sensor = dht.DHT11(Pin(15))
+senslist = []
+def sensor2():
+    try:
+        sleep(2)
+        sensor.measure()
+        temp = sensor.temperature()
+        tempF = temp * (9/5) + 32.0
+        hum = sensor.humidity()
+        senslist = [temp,hum]
+        print(senslist)
+        
+    except OSError as e:
+        print('failed to read sensor.')
+        
+    return temp,hum
 #pins:  cs = 5, vin = 3v3, ground = ground, g0 = 0, sck = 14, miso = 12, mosi = 13
 # Lora Parameters
 RFM95_RST = 27
@@ -17,6 +36,12 @@ lora = LoRa(RFM95_SPIBUS, RFM95_INT, CLIENT_ADDRESS, RFM95_CS, reset_pin=RFM95_R
 
 # loop and send data
 while True:
-    lora.send_to_wait("This is a test message", SERVER_ADDRESS)
+    temp, hum = sensor2()
+    senslist = [temp, hum]
+    sleep(2)
+    sendData = str(senslist)
+    print(type(sendData), sendData, "fra loopen")
+    sleep(2)
+    lora.send_to_wait(sendData, SERVER_ADDRESS)
     print("sent")
-    sleep(10)
+    sleep(5)
