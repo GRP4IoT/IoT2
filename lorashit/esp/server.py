adf7f86262d365e152d8f7bc12bc71ecdf4bb8de
Luxor44 <92293868+Luxor44@users.noreply.github.com>
2022-03-17 12:53:47 +0100

    Create server.py

---
+++ lorashit/esp/server.py
@@ -0,0 +1,31 @@
+from time import sleep
+from ulora import LoRa, ModemConfig, SPIConfig
+
+# This is our callback function that runs when a message is received
+def on_recv(payload):
+    print("From:", payload.header_from)
+    print("Received:", payload.message)
+    print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
+#pins:  cs = 5, vin = 3v3, ground = ground, g0 = 0, sck = 14, miso = 12, mosi = 13
+# Lora Parameter
+RFM95_RST = 27
+RFM95_SPIBUS = SPIConfig.esp32_1
+RFM95_CS = 5
+RFM95_INT = 0
+RF95_FREQ = 868.0
+RF95_POW = 20
+CLIENT_ADDRESS = 222
+SERVER_ADDRESS = 1
+
+# initialise radio
+lora = LoRa(RFM95_SPIBUS, RFM95_INT, SERVER_ADDRESS, RFM95_CS, reset_pin=RFM95_RST, freq=RF95_FREQ, tx_power=RF95_POW, acks=True)
+
+# set callback
+lora.on_recv = on_recv
+
+# set to listen continuously
+lora.set_mode_rx()
+
+# loop and wait for data
+while True:
+    sleep(0.1)
