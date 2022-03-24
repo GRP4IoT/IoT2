ea168a4f4125ad27536ec06376646dd7030ab8ea
MDG-KEA <92229676+MDG-KEA@users.noreply.github.com>
2022-03-24 09:41:03 +0100

    Update lora.py

--- main-files/pi/lora.py
+++ main-files/pi/lora.py
@@ -1,3 +1,4 @@
+from tokenize import _all_string_prefixes
 from pyLoraRFM9x import LoRa, ModemConfig
 # from https://github.com/epeters13/pyLoraRFM9x
 import threading
@@ -5,12 +6,15 @@ import sqlite3
 import datetime
 from time import sleep
 from flask import Flask, render_template
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
 saveToDB = False
 
 #Database connection object
-conn = sqlite3.connect('Desktop/LoneLora/databasen.db')
+conn = sqlite3.connect('Desktop/LoneLora/databasen.db', check_same_thread=False)
 #cursor object
 curs = conn.cursor()
 
@@ -29,11 +33,15 @@ tal = 0
 
 def tableCreation():
     try:
+        conn = sqlite3.connect('Desktop/LoneLora/databasen.db', check_same_thread=False)
+        curs = conn.cursor()
         curs.execute(table)
         print("IOTINFOEN table created or found")
     except sqlite3.Error as e:
         conn.rollback()
         print(f'Could not create table! {e}')
+    finally:
+        conn.close()
 
 if tal == 0:
     tableCreation()
@@ -42,6 +50,8 @@ if tal == 0:
 #Injecte SQL data i databasen
 def insertData(tag, temp, hum, lat, lon):
     try:
+        conn = sqlite3.connect('Desktop/LoneLora/databasen.db', check_same_thread=False)
+        curs = conn.cursor()
         query = "INSERT INTO IOTINFOEN (TAG, DATETIME, TEMPERATURE, HUMIDITY, LATITUDE, LONGITUDE)VALUES(?,?,?,?,?,?)"
         dato = str(datetime.datetime.now())
         dato = dato[0:19]
@@ -54,7 +64,61 @@ def insertData(tag, temp, hum, lat, lon):
     except sqlite3.Error as e:
         conn.rollback()
         print(f'Could not insert! {e}')
-        
+    finally:
+        conn.close()
+
+#Tager de sidste 5 entries ud af SQLite i descending order
+def getData():
+    try:
+        conn = sqlite3.connect('Desktop/LoneLora/databasen.db', check_same_thread=False)
+        curs = conn.cursor()
+        curs.execute('SELECT * FROM IOTINFOEN ORDER BY ID DESC LIMIT 5;')
+        ourData = curs.fetchall()
+        return ourData
+    except sqlite3.Error as e:
+        print(f'Error calling SQL: "{e}"')
+    finally:
+        conn.close()
+
+#Tag alt data ud af SQLite databasen
+def alData():
+    try:
+        conn = sqlite3.connect('Desktop/LoneLora/databasen.db', check_same_thread=False)
+        curs = conn.cursor()
+        curs.execute('SELECT * FROM IOTINFOEN;')
+        allData = curs.fetchall()
+        return allData
+    except sqlite3.Error as e:
+        conn.rollback()
+        print(f'Could not retrieve data! {e}')
+    finally:
+        conn.close()
+
+#Lave koordinat systemer med matplot til at vise vores data
+def matPlots():
+    date = []
+    fugt = []
+    temp = []
+    vorData = alData() #Data udtrækning og valg a den data vi vil have
+    for i in vorData:
+        i[2].append(date)
+    for j in vorData:
+        j[3].append(temp)
+    for u in vorData:
+        u[4].append(fugt)
+    #matplotting
+    fig = Figure()
+    ax = fig.subplots()
+    buf = BytesIO()
+    ax.plot(date, temp)
+    fig.savefig(buf, format="png")
+    data = base64.b64decode(buf.getbuffer()).decode("ascii")
+    aPlot = f"<img src ='data:image/png;base64,{data}'/>"
+    
+    ax.plot(date, fugt)
+    data1 = base64.b64decode(buf.getbuffer()).decode("ascii")
+    bPlot = f"<img src ='data:image/png;base64,{data1}'/>"
+    return aPlot, bPlot
 
 # This is our callback function that runs when a message is received
 def on_recv(payload):
@@ -87,6 +151,8 @@ def on_recv(payload):
 lora = LoRa(1, 17, 222, reset_pin = 25, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
 lora.on_recv = on_recv
 
+# Send a message to a recipient device with address 10
+# Retry sending the message twice if we don't get an acknowledgment from the recipient
 message = "Hello there!"
 status = lora.send_to_wait(message, 1, retries=20)
 if status is True:
@@ -96,7 +162,12 @@ else:
 
 @app.route('/')
 def home():
-   return render_template('home.html')
+    rowData = getData()
+    aPlot, bPlot = matPlots()
+    print(rowData, "fra hjemmeside")
+    print(rowData[0][0], "fra hjemmeside")
+    return render_template('home.html', rowData=rowData, aPlot=aPlot, bPlot=bPlot)
+
 if __name__ == '__main__':
    thread = threading.Thread(target=app.run, args=())
    thread.start()
@@ -106,30 +177,13 @@ print("flask has started")
 while True:
     if saveToDB == True:
         try:
-            print("Trying to save to databse")
+            print("Trying to save to database")
             insertData("device1", ourInts[0], ourInts[1], ourFloats[0], ourFloats[1])
-            saveToDB = False
             print(saveToDB)
+            data = getData()
+            print(data)
+            saveToDB = False
         except:
             print("Couldn't insert data in SQL")
             saveToDB = False
             print(saveToDB)
-
-# Send a message to a recipient device with address 10
-# Retry sending the message twice if we don't get an acknowledgment from the recipient
-"""    message = "Hello there!"
-    status = lora.send_to_wait(message, 1, retries=20)
-    if status is True:
-        print("Message sent!")
-    else:
-        print("No acknowledgment from recipient")
-    if status is False:
-        print("No acknowledgment from recipient")
-    
-    try:
-        print("Our temp and hum readings from ESP: ", ourInts)
-        print("Our GPS readings, lat-lon", ourFloats)
-        insertData("device1", ourInts[0], ourInts[1], ourFloats[0], ourFloats[1])
-        sleep(11) #en sleep for at prøve at stoppe os fra at ligge den samme entry ind flere gange
-    except:
-        print("didn't work")"""
